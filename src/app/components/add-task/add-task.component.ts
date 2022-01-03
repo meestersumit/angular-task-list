ae61d8daa3586576fedd2efa269acab2664d7e91
meestersumit <DchAdNGfyDMM7j8>
2022-01-03 15:19:40 -0700

    angular crash course for task list

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css'],
+})
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
+  showAddTask!: boolean;
+  subsription!: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subsription = this.uiService.onToggle().subscribe((value) => {
+      this.showAddTask = value;
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (!this.text) {
+      alert('please add a task');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    //emit event
+    this.onAddTask.emit(newTask);
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
+}
